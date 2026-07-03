14a6572ed1a2e36b79e4aafb41469683ecc08398
pengmengying2025 <mengying.peng25@gmail.com>
2026-07-03 09:08:57 +0000

    Deploying to gh-pages from @ pengmengying2025/pengmengying2025.github.io@d8e4148c8ab2c07820c35558183c5e5fd0c0c65c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
